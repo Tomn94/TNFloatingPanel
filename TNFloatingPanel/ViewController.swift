5a22b9ba155818b3932db9b1a1cc4d3dc1edef89
tomn94 <tmnstudios@hotmail.fr>
2017-07-07 19:46:26 +0200

    Initial Commit

---
+++ TNFloatingPanel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TNFloatingPanel
+//
+//  Created by Tomn on 07/07/2017.
+//  Copyright © 2017 Thomas NAUDET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
